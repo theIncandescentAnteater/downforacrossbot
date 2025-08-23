dcd3e12af4ddc907c86d236ab2cee952544b04a2
Kelsey Goldwein <kmg94@duke.edu>
2025-08-22 20:50:57 -0400

    sample embed code from before removal from main

--- main.py
+++ main.py
@@ -35,7 +35,20 @@ client = Client(command_prefix='!', intents=intents) # prefix sorta irrelevant, 
 # pull values from .env
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
+# puzzleRoleName = os.getenv('PUZZLE_ROLE')
 
+# @client.tree.command(name="pingme", description="change if you get pinged when a puzzle is posted")
+# async def pingme(interaction: discord.Interaction, toggle: Literal["yes", "no"]):
+#     role = discord.utils.get(interaction.guild.roles, name=puzzleRoleName)
+#     if role:
+#         if toggle == "yes":
+#             await interaction.user.add_roles(role)
+#             await interaction.response.send_message(f"{interaction.user.mention} will now be pinged for puzzles", ephemeral = True)
+#         else:
+#             await interaction.user.remove_roles(role)
+#             await interaction.response.send_message(f"{interaction.user.mention} will no longer be pinged for puzzles", ephemeral = True)
+#     else:
+#         await interaction.response.send_message("Role doesn't exist")
 
 @client.tree.command(name="puzzle", description="start a puzzle")
 async def startPuzzle(interaction: discord.Interaction, 
@@ -58,6 +71,34 @@ async def startPuzzle(interaction: discord.Interaction,
             await interaction.response.send_message(f"no puzzles found for {puzzleName}", ephemeral=True)
         else:
             await interaction.response.send_message(game)
+
+    except Exception as e:
+        print(f"Error getting results: {e}")
+
+@client.tree.command(name="puzzleembed", description="start a puzzle")
+async def startPuzzleEmbed(interaction: discord.Interaction, 
+                        publisher: Literal["nyt", "lat", "usa", "wsj", "newsday", "universal", "atlantic"],
+                        date: str = ""):
+    try:
+        dateFormat = re.compile(r"^[0-1]?\d/[0-3]?\d(/[1-2]\d\d\d)?$")
+
+        if dateFormat.match(date):
+            dateParts = date.split("/")
+            year = datetime.date.today().year if len(dateParts) == 2 else int(dateParts[2])
+
+            puzzleDate = datetime.date(year, int(dateParts[0]), int(dateParts[1]))
+            puzzleName = puzzle_utils.getPuzzleName(publisher, puzzleDate)
+        else:
+            puzzleName = puzzle_utils.getPuzzleName(publisher)
+
+        game = await puzzle_utils.makeGame(searchTerm = puzzleName)
+        if game == None:
+            await interaction.response.send_message(f"no puzzles found for {puzzleName}", ephemeral=True)
+        else:
+            # puzzleRole = discord.utils.get(interaction.guild.roles, name=puzzleRoleName)
+            await interaction.response.send_message(embed=discord.Embed(title=puzzleName, url=game, color=discord.Color.from_str("#78a6ee")\
+                                                                        # , description=f"<@&{puzzleRole.id}>"\
+                                                                            ))
 
     except Exception as e:
         print(f"Error getting results: {e}")
