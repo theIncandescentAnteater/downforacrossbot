ea86d0db397f19197e52936f9fe73de44d71fcaa
Kelsey Goldwein <kmg94@duke.edu>
2025-12-28 17:07:29 -0500

    add optional publisher argument to puzzle command, that sends todays puzzle from that publisher rather than button menu

--- commands/puzzle.js
+++ commands/puzzle.js
@@ -1,23 +1,43 @@
 const { publisherButtons } = require('../utilities/buttons');
+const { sendPuzzle } = require('../utilities/puzzle-utils');
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('puzzle')
-		.setDescription('start a puzzle with crosswithfriends'),
+		.setDescription('start a puzzle with crosswithfriends')
+		.addStringOption((publisher) => 
+			publisher
+				.setName('publisher')
+				.setDescription('the publisher that puzzle came from')
+				.addChoices(
+					{ name: 'nyt', value: 'nyt' },
+					{ name: 'lat', value: 'lat' },
+					{ name: 'usa', value: 'usa' },
+					{ name: 'wsj', value: 'wsj' },
+					{ name: 'newsday', value: 'newsday' },
+					{ name: 'universal', value: 'universal' },
+					{ name: 'new yorker', value: 'new yorker' },
+				)
+		),
 
 	async execute(interaction) {
 
 		try {
-			const puzzleEmbed = new EmbedBuilder()
-				.setColor(0x91a774)
-				.setDescription('play one of today\'s puzzles?');
+			const publisher = interaction.options.getString('publisher');
 
-			await interaction.reply({ 
-				embeds: [puzzleEmbed],
-				components: [publisherButtons],
-			 });
+			if (!publisher){
+				const puzzleEmbed = new EmbedBuilder()
+					.setColor(0x91a774)
+					.setDescription('play one of today\'s puzzles?');
 
+				await interaction.reply({ 
+					embeds: [puzzleEmbed],
+					components: [publisherButtons],
+				});
+			} else {
+				await sendPuzzle(interaction, publisher);
+			}
 		} catch (error) {
 			console.error(error.message);
 		}
